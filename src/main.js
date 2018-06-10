66ca172e74c677d79364d6b048fe1b28c5f8cc38
Floretta <Moonlington@users.noreply.github.com>
2018-06-10 18:39:47 +0200

    ADD: Upgrade button OwO

--- src/main.js
+++ src/main.js
@@ -1,17 +1,41 @@
 window.onload = init;
 
 let fps = 60;
-
-var vircounter = 0;
+var Game = {
+  virCounter: 0,
+  addButtonUpgradeUnlocked: false,
+  addButtonUpgrades:1
+}
 
 function init() {
-  document.getElementById('upbutton1').onclick = function(){
-    vircounter++;
+  document.getElementById('addButton').onclick = function(){
+    Game.virCounter += 1*Game.addButtonUpgrades;
+  }
+  document.getElementById('upgradeAddButton').onclick = function(){
+    if (!this.classList.contains("disabled")) {
+      Game.virCounter -= 9+1*Game.addButtonUpgrades
+      Game.addButtonUpgrades++;
+    }
   }
 
   window.setInterval(gameLoop, 1000/fps)
 }
 
 function gameLoop() {
-  document.getElementById('vircounter').innerHTML = vircounter
+  document.getElementById('virCounter').innerHTML = Game.virCounter
+  document.getElementById('addButton').innerHTML = "Add " + 1*Game.addButtonUpgrades + " Vir"
+  document.getElementById('upgradeAddButton').innerHTML = "Upgrade Vir: " + (9+1*Game.addButtonUpgrades) + " Vir"
+  if (Game.virCounter >= 10) {
+    document.getElementById('upgradeAddButton').classList.remove("hidden")
+    Game.addButtonUpgradeUnlocked = true;
+  }
+  if (Game.virCounter >= 9+1*Game.addButtonUpgrades) {
+    if (document.getElementById('upgradeAddButton').classList.contains("disabled")) {
+      document.getElementById('upgradeAddButton').classList.remove("disabled")
+    }
+  } else {
+    if (!document.getElementById('upgradeAddButton').classList.contains("disabled")) {
+      document.getElementById('upgradeAddButton').classList.add("disabled")
+    }
+  }
 }
